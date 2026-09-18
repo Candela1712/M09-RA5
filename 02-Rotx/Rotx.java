a1229c304106c8da852cd4ea2832ac8b774f6c71
Candela1712 <candela.ortuno.l@gmail.com>
2026-09-18 18:06:56 +0200

    Create Rotx.java

---
+++ 02-Rotx/Rotx.java
@@ -0,0 +1,17 @@
+public class Rotx {
+    static String abc = "aáàbcçdeéèfghiíìïjklmnñoóòpqrstuúùüwxyz";
+    static char[] minuscules = abc.toCharArray();
+    static char[] majuscules = abc.toUpperCase().toCharArray();
+
+
+    public static String xifraRot(String cadena, int desplacament){
+        String textXifrat = "";
+
+        return textXifrat;
+    }
+    public static String desxifraRot( String cadena, int desplacament){
+        String textDesxifrat = "";
+        
+        return textDesxifrat;
+    }
+}
